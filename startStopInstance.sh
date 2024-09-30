52ad02a9d17885b6a65689c673d8b10e1292d293
EC2 Default User <ec2-user@ip-172-31-5-109.us-east-2.compute.internal>
2024-09-30 03:34:58 +0000

    final version

--- startStopInstance.sh
+++ startStopInstance.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-ami_id = ami-037774efca2da0726
 
-instance_id = $(aws ec2 run-instances --image-id $ami_id --count 1 --instance-type t2.micro --key-name ohio-public-key  --region us-east-2 —query Instances.InstanceId --output text)
+id_for_ami="ami-037774efca2da0726"
 
-aws ec2 terminate-instances —instance-ids $nstance_id —region us-east-2
+
+instance_id=$(aws ec2 run-instances \
+    --image-id $id_for_ami \
+    --count 1 \
+    --instance-type t2.micro \
+    --key-name ohio-public-key \
+    --region us-east-2 \
+    --query 'Instances[0].InstanceId' \
+    --output text)
+
+
+sleep 60
+
+# stoping the instance
+aws ec2 terminate-instances --instance-ids $instance_id --region us-east-2
+
